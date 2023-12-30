6e0d27e103a2b028c2c20611763edbb9e7ac1553
Sreelal TS <heysreelal@gmail.com>
2023-12-30 19:51:17 +0530

    🚀 That's all I want

--- bin/xsend.dart
+++ bin/xsend.dart
@@ -1,5 +1,21 @@
+import 'package:args/args.dart';
 import 'package:xsend/xsend.dart';
 
 void main(List<String> args) {
-  bot;
+  final ArgParser parser = ArgParser();
+  parser.addOption(
+    "path",
+    abbr: "p",
+    help: "Path to the file to send",
+    mandatory: true,
+  );
+
+  parser.addOption(
+    "chat",
+    abbr: "c",
+    help: "Chat ID to send the file to",
+  );
+
+  final results = parser.parse(args);
+  send(results);
 }
--- lib/xsend.dart
+++ lib/xsend.dart
@@ -1,4 +1,18 @@
 import 'dart:io';
+import 'package:args/args.dart';
 import 'package:televerse/televerse.dart';
 
 Bot bot = Bot(Platform.environment['BOT_TOKEN']!);
+
+Future<void> send(ArgResults results) async {
+  final path = results['path'] as String;
+  ID chat;
+  if (results['chat'] == null) {
+    chat = ChatID(int.parse(Platform.environment['CHAT_ID']!));
+  } else {
+    chat = ID.create(results['chat'] as String);
+  }
+
+  await bot.api.sendDocument(chat, InputFile.fromFile(File(path)));
+  print("File sent!");
+}
