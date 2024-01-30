bab37e7c5668f57129d4deff9cb4e2d08c6a90b9
Sreelal TS <heysreelal@gmail.com>
2024-01-30 16:21:43 +0530

    🫥 Additional checks for binary data

--- lib/xsend.dart
+++ lib/xsend.dart
@@ -8,9 +8,24 @@ Future<void> send(Args args) async {
   final file = File(args.path);
 
   if (args.isContent) {
+    String content;
+    try {
+      content = file.readAsStringSync();
+    } catch (e) {
+      if (e is FileSystemException) {
+        if (e.message == "Failed to decode data using encoding 'utf-8'") {
+          print("You cannot send binary files in content mode.");
+        } else {
+          print("An error occured while reading the file.");
+          print(e.message);
+        }
+      }
+      content = "Could not read file.";
+    }
+
     await bot.api.sendMessage(
       args.chat,
-      "```${args.fileExt}\n${file.readAsStringSync()}```",
+      "```${args.fileExt}\n$content```",
       parseMode: ParseMode.markdownV2,
     );
   } else {
